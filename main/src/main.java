0efbffcdf747a419b89e78c8b159d06906a20148
AndrewEvl <Lino220037@gmail.com>
2017-05-22 20:04:33 +0300

    Task 2

--- main/src/main.java
+++ main/src/main.java
@@ -4,7 +4,7 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hi");
-        System.out.println("Hi, or goodbye");
+        System.out.println("Hi, or goodbye!");
 
     }
 }
