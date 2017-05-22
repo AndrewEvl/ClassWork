813c8bcc0dda5debf3419a0160ea9155cce807c5
AndrewEvl <Lino220037@gmail.com>
2017-05-22 20:04:15 +0300

    Git start

--- .idea/main/src/main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by User on 22.05.2017.
- */
-public class main {
-    public static void main(String[] args) {
-        System.out.println("Hi");
-    }
-}
--- main/src/main.java
+++ main/src/main.java
@@ -4,7 +4,7 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hi");
-        System.out.println("Hi, hi");
+        System.out.println("Hi, or goodbye");
 
     }
 }
