b5a4da61077de1be330a37df33e06463cf485d57
AndrewEvl <Lino220037@gmail.com>
2017-05-22 20:09:41 +0300

    Task 2

--- main/src/main.java
+++ main/src/main.java
@@ -5,7 +5,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hi");
         System.out.println("Hi, hi from kate");
-        System.out.println("Hi, hi from kate");
+        System.out.println("Hi, hi from kate, hi");
 
     }
 }
