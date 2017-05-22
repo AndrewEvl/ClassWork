c633aeb0b0cd4e60f17b0825082c3889e085f570
Kate <kate@gmail.com>
2017-05-22 21:34:22 +0300

    message
    (cherry picked from commit 05578b2)

--- main/src/main.java
+++ main/src/main.java
@@ -4,9 +4,7 @@
 public class main {
     public static void main(String[] args) {
 
-        System.out.println("this is second branch");
-        System.out.println("test");
-        System.out.println("Pull Test");
+      //1
 
     }
 }
